8735e765f66ddae45fd7082050a7572ad0b0b2e8
Fatemeh Sadat Hoseini <fatemehoseinii007@gmail.com>
2025-09-27 09:10:56 +0330

    I improved my final project

--- script.js
+++ script.js
@@ -9,23 +9,22 @@
    cursor: "",
   delay: 90
 });
-
-
  }
  
  function poemGenerator(Event){
     Event.preventDefault();
 
-    thirdHeader.innerHTML="It is generating poem..."
     let txtInput=document.querySelector("#txt-input")
     let myApi="c788bbe4fbob842f25a727737f00tc5e"
     let  prompt=`generate me a poem in max 4 line in persian then write that poem on English bellow of persian poem it must be about ${txtInput.value}`
     let context=`it must be max 4 line first it should be in persian then English it should be popular Irainian poem and also use <br> for big spaces between poem lines and aso use<br><br> for wriiting English one and the poems must has right grammer and they must be sensetive and pretty,and at the bottem of all poem write 'more information about persian poems...' at put it in <strong></string> and before it use <br><br> and also in strong tag use <a target="_blank" href="https://en.wikipedia.org/wiki/Persian_literature"></a>`
     let url=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${myApi}`
-    
+    thirdHeader.innerHTML=`⏳ It is generating poem about ${txtInput.value}`
+    mainPart.style.display="block"
     axios.get(url).then(aiPoem)
-
 }
 let thirdHeader=document.querySelector("#third-header")
 let formElement=document.querySelector("#form")
+let mainPart=document.querySelector("#main")
 formElement.addEventListener("submit",poemGenerator)
+mainPart.style.display="none"
